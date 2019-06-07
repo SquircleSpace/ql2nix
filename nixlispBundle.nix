0b95af727422d7d1a0b3a4964bfa5cfde42f292c
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2019-06-07 09:37:32 -0700

    nixlisp bundles should use clwrapper
    If a nixlisp bundle is listed as a buildInput then the contained lisp
    libraries should be part of the ASDF registry.  clwrapper has a hook
    that sets that up.  We just need to participate in clwrapper's
    scheme.

--- nixlispBundle.nix
+++ nixlispBundle.nix
@@ -17,9 +17,8 @@ let
 in mkDerivation rec {
   name = "nixlisp-bundle-${version}";
   version = "1.0.0";
+  propagatedBuildInputs = [ clwrapper ];
   unpackPhase = "true";
-  bundleIt = ''
-  '';
   buildPhase = ''
     mkdir -p quicklisp/tmp
     mkdir -p quicklisp/local-projects
@@ -35,22 +34,33 @@ in mkDerivation rec {
 
     ${nixlispDist}/bin/nixlisp-installer quicklisp/dists/nixlisp/
 
-    ASDF_OUTPUT_TRANSLATIONS="(:output-translations :ignore-inherited-configuration (t :here))"
-    export ASDF_OUTPUT_TRANSLATIONS
-
     # Sourcing this file with NIX_LISP_SKIP_CODE=1 always returns
     # non-zero.  Since we're building in a set -e environment we need to
     # cause that non-zero return value to become zero.
     NIX_LISP_SKIP_CODE=1 source "${clwrapper}/bin/common-lisp.sh" || true
     mkdir -p bundle
     "${clwrapper}/bin/common-lisp.sh" "$NIX_LISP_LOAD_FILE" "${bundler}"
-  '';
-  installPhase = ''
-    mkdir -p "$out"
-    tar -c -C bundle/ . | tar -x -C "$out"
+
     # This file contains nondeterministic output (e.g. date and info
     # about host).  Until we have some reason to include it (or a
     # replacement), its better to leave it out.
-    rm "$out/bundle-info.sexp"
+    rm "bundle/bundle-info.sexp"
+  '';
+  installPhase = ''
+    mkdir -p "$out/lib/common-lisp"
+    mv bundle "$out/lib/common-lisp/"
+
+    mkdir -p "$out/lib/common-lisp-settings"
+    outhash="$out"
+    outhash="''${outhash##*/}"
+    outhash="''${outhash%%-*}"
+    cat > "$out/lib/common-lisp-settings/bundle-path-config.sh" <<EOF
+    if [ -z "\''${_''${outhash}_NIX_LISP_PATH_CONFIG}" ]; then
+    export _''${outhash}_NIX_LISP_PATH_CONFIG=1
+    export NIX_LISP_ASDF_PATHS="$NIX_LISP_ASDF_PATHS
+    $out/lib/common-lisp/bundle"
+    fi
+    EOF
+    chmod +x "$out/lib/common-lisp-settings/bundle-path-config.sh"
   '';
 }
