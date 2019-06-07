bb36ca21c73e7d7ceb8ba1d9cc4299ddfcae98a9
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2019-06-07 09:37:46 -0700

    Make mkNixlispBundle callable using nixpkgs as an argument
    This is meant to be the convenient wrapper that puts the pieces
    together for you!

--- mkNixlispBundle.nix
+++ mkNixlispBundle.nix
@@ -1,6 +1,14 @@
-{ writeTextFile, concatMapStrings, mkDerivation, quicklisp, clwrapper }:
+{ writeTextFile, lib, stdenv, lispPackages, ... }:
 qlDist:
 let
-  nixlispDist = import ./nixlispDist.nix { inherit writeTextFile concatMapStrings mkDerivation qlDist; };
-  nixlispBundle = import ./nixlispBundle.nix { inherit nixlispDist quicklisp clwrapper writeTextFile mkDerivation; };
+  concatMapStrings = lib.concatMapStrings;
+  mkDerivation = stdenv.mkDerivation;
+  quicklisp = lispPackages.quicklisp;
+  clwrapper = lispPackages.clwrapper;
+  nixlispDist = import ./nixlispDist.nix {
+    inherit writeTextFile concatMapStrings mkDerivation qlDist;
+  };
+  nixlispBundle = import ./nixlispBundle.nix {
+    inherit nixlispDist quicklisp clwrapper writeTextFile mkDerivation;
+  };
 in nixlispBundle
